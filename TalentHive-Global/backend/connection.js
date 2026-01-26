674075c3e33022fc3d0424566064d6c76c70cc85
9802HEMENSAN <hmahilange9802@gmail.com>
2026-01-26 12:40:07 +0530

    package.json updated

--- TalentHive-Global/backend/connection.js
+++ TalentHive-Global/backend/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
+//  TalentHive-Global
 
-// linkedInClone
-
-mongoose.connect('mongodb://localhost:27017/linkedInClone').then(res=>{
+mongoose.connect('mongodb://localhost:27017/').then(res=>{
     console.log("Database Successfully connected")
 }).catch(err=>{
     console.log(err)
